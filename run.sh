d381e067757d1cbb924a2c114b1312ce90b4f331
김채현 <108905986+chaehyun1@users.noreply.github.com>
2025-01-28 00:28:40 +0900

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python main.py \
+  --dataset="CAMRa2011" \
+  --verbose=0 \
+  --alpha=0.3 \
+  --beta=0.3 \
+  --power=0.9 \
+  --user_filter=1 \
+  --group_filter=2 \
+  --uni_filter=3 \
+  --top_k=10
